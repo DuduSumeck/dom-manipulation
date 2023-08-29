3027caac24da2d937e4432f96c649f341b2ecc90
Luiz Azevedo <dudusumeck@gmail.com>
2023-08-28 21:00:14 -0300

    bookmark movie

--- index.js
+++ index.js
@@ -1,123 +1,138 @@
 (() => {
   const form = document.querySelector(".search-form");
-  const input = document.querySelector(".search-input");
-
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
+    const input = document.querySelector(".search-input");
     const query = input.value.trim();
     if (query) {
-      const movies = document.querySelector(".movies");
-      movies.innerHTML = "";
-
-      fetchMovies(`https://api.themoviedb.org/3/search/movie?query=${query}`);
+      url = `https://api.themoviedb.org/3/search/movie?query=${query}`;
+      const movies = await fetchMovies(url);
+      renderMovies(movies);
     }
   });
 })();
 
-fetchMovies();
+document.addEventListener("DOMContentLoaded", async () => {
+  const url =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+  const movies = await fetchMovies(url);
+  renderMovies(movies);
+});
 
-async function fetchMovies(
-  url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
-) {
-  try {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjE5NGMzNDFlNTk3OGNlNmU1ZDlkOGE4NTYzOGQxYSIsInN1YiI6IjY0YzU3NmRmZWVjNWI1MDBlMjNiYmEzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AQV8GQMvfS7PByxBHJY0itt9PiuVVn2TJt3If6SxxAM",
-      },
-    };
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-
-    const data = await response.json();
-    populateMovies(data);
-  } catch (error) {
-    console.error(`Could not get movies: ${error}`);
-  }
-}
-
-function populateMovies(data) {
-  data.results
-    .map((result) => parseMovie(result))
-    .forEach((movie) => renderMovie(movie));
-}
-
-function parseMovie(result) {
-  return {
-    image: `https://image.tmdb.org/t/p/w500/${result.poster_path}`,
-    title: result.title,
-    year: new Date(result.release_date).getFullYear(),
-    rating: result.vote_average,
-    description: result.overview,
+async function fetchMovies(url) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjE5NGMzNDFlNTk3OGNlNmU1ZDlkOGE4NTYzOGQxYSIsInN1YiI6IjY0YzU3NmRmZWVjNWI1MDBlMjNiYmEzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AQV8GQMvfS7PByxBHJY0itt9PiuVVn2TJt3If6SxxAM",
+    },
   };
+  const response = await fetch(url, options);
+  const { results } = await response.json();
+  return results;
 }
 
-function renderMovie(movie) {
-  const movies = document.querySelector(".movies");
+function renderMovies(movies) {
+  const moviesContainer = document.querySelector(".movies");
+  moviesContainer.innerHTML = "";
 
-  const movieContainer = document.createElement("div");
-  movieContainer.classList.add("movie-container");
+  movies.forEach((movie) => {
+    const { poster_path, title, release_date, vote_average, overview, id } =
+      movie;
+    const year = new Date(release_date).getFullYear();
+    const image = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
-  const movieCard = document.createElement("div");
-  movieCard.classList.add("movie-card");
+    const movies = document.querySelector(".movies");
 
-  const movieIcon = document.createElement("img");
-  movieIcon.classList.add("movie-icon");
-  movieIcon.setAttribute("src", movie.image);
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
 
-  movieIcon.addEventListener("error", () => {
-    movieIcon.setAttribute("src", "/images/film.png");
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    const movieIcon = document.createElement("img");
+    movieIcon.classList.add("movie-icon");
+    movieIcon.setAttribute("src", image);
+
+    movieIcon.addEventListener("error", () => {
+      movieIcon.setAttribute("src", "/images/film.png");
+    });
+
+    movieCard.appendChild(movieIcon);
+
+    const movieDetails = document.createElement("div");
+    movieDetails.classList.add("movie-details");
+
+    const movieTitle = document.createElement("h2");
+    movieTitle.classList.add("movie-title");
+    const titleText = document.createTextNode(`${title} (${year})`);
+    movieTitle.appendChild(titleText);
+
+    movieDetails.appendChild(movieTitle);
+
+    const movieData = document.createElement("div");
+    movieData.classList.add("movie-data");
+
+    const movieRating = document.createElement("span");
+    movieRating.classList.add("movie-rating");
+    const ratingText = document.createTextNode(vote_average);
+    movieRating.appendChild(ratingText);
+
+    movieData.appendChild(movieRating);
+
+    const movieBookmark = document.createElement("span");
+    movieBookmark.classList.add("movie-bookmark");
+    movieBookmark.dataset.movieId = id;
+
+    const bookmarkedMovies = getBookmarkedMovies();
+    movieBookmark.classList.toggle(
+      "movie-bookmarked",
+      bookmarkedMovies.includes(id)
+    );
+
+    movieBookmark.addEventListener("click", (e) => {
+      const movieId = parseInt(movieBookmark.dataset.movieId);
+      toogleBookmark(e.target, movieId);
+    });
+
+    const bookmarkText = document.createTextNode("Favoritar");
+    movieBookmark.appendChild(bookmarkText);
+
+    movieData.appendChild(movieBookmark);
+
+    movieDetails.appendChild(movieData);
+
+    movieCard.appendChild(movieDetails);
+
+    movieContainer.appendChild(movieCard);
+
+    const movieDescription = document.createElement("p");
+    movieDescription.classList.add("movie-description");
+    const descriptionText = document.createTextNode(overview);
+    movieDescription.appendChild(descriptionText);
+
+    movieContainer.appendChild(movieDescription);
+
+    movies.appendChild(movieContainer);
   });
+}
 
-  movieCard.appendChild(movieIcon);
+function toogleBookmark(bookmark, movieId) {
+  const bookmarkedMovies = getBookmarkedMovies();
+  const index = bookmarkedMovies.indexOf(movieId);
+  if (index === -1) bookmarkedMovies.push(movieId);
+  else bookmarkedMovies.splice(index, 1);
 
-  const movieDetails = document.createElement("div");
-  movieDetails.classList.add("movie-details");
+  bookmark.classList.toggle(
+    "movie-bookmarked",
+    bookmarkedMovies.includes(movieId)
+  );
 
-  const movieTitle = document.createElement("h2");
-  movieTitle.classList.add("movie-title");
-  const titleText = document.createTextNode(`${movie.title} (${movie.year})`);
-  movieTitle.appendChild(titleText);
+  localStorage.setItem("bookmarkedMovies", JSON.stringify(bookmarkedMovies));
+}
 
-  movieDetails.appendChild(movieTitle);
-
-  const movieData = document.createElement("div");
-  movieData.classList.add("movie-data");
-
-  const movieRating = document.createElement("span");
-  movieRating.classList.add("movie-rating");
-  const ratingText = document.createTextNode(movie.rating);
-  movieRating.appendChild(ratingText);
-
-  movieData.appendChild(movieRating);
-
-  const movieBookmark = document.createElement("span");
-  movie.isBookmarked
-    ? movieBookmark.classList.add("movie-bookmarked")
-    : movieBookmark.classList.add("movie-bookmark");
-  const bookmarkText = document.createTextNode("Favoritar");
-  movieBookmark.appendChild(bookmarkText);
-
-  movieData.appendChild(movieBookmark);
-
-  movieDetails.appendChild(movieData);
-
-  movieCard.appendChild(movieDetails);
-
-  movieContainer.appendChild(movieCard);
-
-  const movieDescription = document.createElement("p");
-  movieDescription.classList.add("movie-description");
-  const descriptionText = document.createTextNode(movie.description);
-  movieDescription.appendChild(descriptionText);
-
-  movieContainer.appendChild(movieDescription);
-
-  movies.appendChild(movieContainer);
+function getBookmarkedMovies() {
+  return JSON.parse(localStorage.getItem("bookmarkedMovies")) || [];
 }
